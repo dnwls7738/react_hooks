26ca13a44c22801164bf2278181dc4ea045bf193
dnwls7738 <cjsdnwls7738@gmail.com>
2023-10-16 17:55:09 +0900

    라우터수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
-import { BrowserRouter as Route, BrowserRouter, Switch } from "react-router-dom";
+import {
+	BrowserRouter as Route,
+	HashRouter,
+	Switch,
+} from "react-router-dom";
 import State from "./components/State";
 import Input from "./components/Input";
 import Main from "./components/Main";
@@ -19,61 +23,61 @@ import Notification from "./components/Notification";
 import Axios from "./components/Axios";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Switch>
-        <Route path="/" exact>
-          <Main />
-        </Route>
-        <Route path="/state" exact>
-          <State />
-        </Route>
-        <Route path="/input" exact>
-          <Input />
-        </Route>
-        <Route path="/tab" exact>
-          <Tab />
-        </Route>
-        <Route path="/effect" exact>
-          <Effect />
-        </Route>
-        <Route path="/title" exact>
-          <Title />
-        </Route>
-        <Route path="/click" exact>
-          <Click />
-        </Route>
-        <Route path="/confirm" exact>
-          <Confirm />
-        </Route>
-        <Route path="/preventleave" exact>
-          <PreventLeave />
-        </Route>
-        <Route path="/beforeleave" exact>
-          <BeforeLeave />
-        </Route>
-        <Route path="/fade" exact>
-          <Fade />
-        </Route>
-        <Route path="/network" exact>
-          <Network />
-        </Route>
-        <Route path="/scroll" exact>
-          <Scroll />
-        </Route>
-        <Route path="/fullscreen" exact>
-          <Fullscreen />
-        </Route>
-        <Route path="/notification" exact>
-          <Notification />
-        </Route>
-        <Route path="/axios" exact>
-          <Axios />
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  );
+	return (
+		<HashRouter>
+			<Header />
+			<Switch>
+				<Route path="/" exact>
+					<Main />
+				</Route>
+				<Route path="/state" exact>
+					<State />
+				</Route>
+				<Route path="/input" exact>
+					<Input />
+				</Route>
+				<Route path="/tab" exact>
+					<Tab />
+				</Route>
+				<Route path="/effect" exact>
+					<Effect />
+				</Route>
+				<Route path="/title" exact>
+					<Title />
+				</Route>
+				<Route path="/click" exact>
+					<Click />
+				</Route>
+				<Route path="/confirm" exact>
+					<Confirm />
+				</Route>
+				<Route path="/preventleave" exact>
+					<PreventLeave />
+				</Route>
+				<Route path="/beforeleave" exact>
+					<BeforeLeave />
+				</Route>
+				<Route path="/fade" exact>
+					<Fade />
+				</Route>
+				<Route path="/network" exact>
+					<Network />
+				</Route>
+				<Route path="/scroll" exact>
+					<Scroll />
+				</Route>
+				<Route path="/fullscreen" exact>
+					<Fullscreen />
+				</Route>
+				<Route path="/notification" exact>
+					<Notification />
+				</Route>
+				<Route path="/axios" exact>
+					<Axios />
+				</Route>
+			</Switch>
+		</HashRouter>
+	);
 }
 
 export default App;
